a93fa0a87912425825f657f649ac012c4be62e18
developerinc <chivingtoninc@icloud.com>
2017-10-31 23:10:30 -0700

    Initial Commit

---
+++ 1.overview.js
@@ -0,0 +1,20 @@
+// ------------------- Overview ------------------- //
+/*
+  - First, if you have other awesome developer tool suggestions, put
+    those in the comments below.
+
+  - This series is meant for those who are new to development
+    or are transitioning from another area of development, and are not already
+    familiar with these tools at all.
+
+  - I want to make sure you guys are in the right spot and not wasting time on
+    videos that will provide you no value.
+
+  - If you're already experienced even slightly with these tools, this series
+    will do nothing for you.
+
+  - This is NOT the deep dive on these tools. This is download/installation, recommended
+    setup/configuration, and basic commands/usage.
+
+  - Thank, subscribe, like, questions/comments, see you next time.
+*/
---
+++ 2.terminal.js
@@ -0,0 +1,65 @@
+// ------------------- Preliminary Info ------------------- //
+/*
+  - Will work on Mac/Linux
+  - Some will work in Windows PWR Shell, NOT regular CMD Prompt
+  - Don't have a Windows CMD/PWR Shell vid, but...
+    - Shane Young
+    - https://www.youtube.com/watch?v=IHrGresKu2w
+*/
+
+
+// ------------------- Overview ------------------- //
+/*
+  - The Terminal is a built-in command line interface for your computer.
+  - Basically, you just type your commands rather than point/click.
+  - There are a ton of other commands, these are the "Top 10 Most Helpful"
+
+  - Download @: Already installed by default on every computer.
+  - Documentation @:
+    - Mac: (Cheatsheets)
+      - https://www.git-tower.com/blog/command-line-cheat-sheet/
+      - https://learntocodewith.me/command-line/unix-command-cheat-sheet/
+    - Linux:
+      - https://help.ubuntu.com/community/UsingTheTerminal
+
+  - ADHDevvv Info @:
+    - This video @: https://youtube.com/...
+    - Medium article @: https://medium.com/@adhdevvv/...
+*/
+
+
+// ------------------- Commands ------------------- //
+
+// ==== LS ==== //
+// Command: ls
+// Common Flags: -l -a -la
+
+// ==== CLEAR ==== //
+// Command: clear
+
+// ==== CD ==== //
+// Command: cd /path/to/change/into
+// Misc: "." & ".."
+
+// ==== CAT ==== //
+// Command: cat filename.ext
+
+// ==== Nano ==== //
+// Command: nano filename.ext
+
+// ==== MKDIR ==== //
+// Command: mkdir /path/to/dirname
+
+// ==== RM ==== //
+// Command: rm filename.ext
+// Common Flags: -R
+
+// ==== CP ==== //
+// Command: cp filename.ext /path/to/location
+// Common Flags: -R
+
+// ==== MV ==== //
+// Command: mv filename.ext
+
+// ==== SUDO ==== //
+// Command: sudo yourCommand
